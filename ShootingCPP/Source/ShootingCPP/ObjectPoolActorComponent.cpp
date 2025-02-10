6753f9ec2aef00b78a88d95807de361adcb07b0e
go2665 <77224762+go2665@users.noreply.github.com>
2025-02-10 10:01:29 +0900

    ShootingCPP - 오브젝트 풀 액터 컴포넌트 만들기

--- ShootingCPP/Source/ShootingCPP/ObjectPoolActorComponent.cpp
+++ ShootingCPP/Source/ShootingCPP/ObjectPoolActorComponent.cpp
@@ -8,7 +8,7 @@ UObjectPoolActorComponent::UObjectPoolActorComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 	/*
@@ -28,16 +28,72 @@ void UObjectPoolActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	Pool.Empty(InitialPoolSize);	// Pool 비우기(InitialPoolSize 크기로 초기화)
+	if (PoolingObjectClass)			// PoolingObjectClass가 지정되어 있다면 생성 시작	
+	{
+		for (int32 i = 0; i < InitialPoolSize; i++)
+		{
+			SpawnNewPooledObject();	// InitialPoolSize만큼 생성
+		}
+	}	
 }
 
-
-// Called every frame
-void UObjectPoolActorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+APoolingObjectBase* UObjectPoolActorComponent::GetObject(const FTransform& Transform)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	APoolingObjectBase* PooledObject = nullptr;	// 반환할 오브젝트
 
-	// ...
+	if (Pool.Num() > 0)	// Pool에 오브젝트가 있으면
+	{
+		PooledObject = Pool.Pop(false);	// Pool에서 꺼내온다.(주의! - 리사이즈 테스트 필요)
+	}
+	else
+	{
+		PooledObject = SpawnNewPooledObject();	// Pool이 비어있으면 새로 생성
+	}
+
+	return PooledObject;
+}
+
+APoolingObjectBase* UObjectPoolActorComponent::GetObject()
+{
+	return GetObject(FTransform::Identity);	// GetObject(FTransform&) 호출
+}
+
+APoolingObjectBase* UObjectPoolActorComponent::SpawnNewPooledObject()
+{
+	AActor* Owner = GetOwner();	// 이 컴포넌트가 들어있는 액터를 가져오기
+	if (!Owner)
+	{
+		return nullptr;			// 오너가 없으면 null을 리턴하고 종료
+	}
+
+	FActorSpawnParameters SpawnParams;	// 스폰 파라미터 생성
+	SpawnParams.Owner = Owner;			// 스폰 파라미터의 오너를 Owner로 설정
+	UWorld* World = GetWorld();			// 월드 가져오기
+	APoolingObjectBase* NewPoolObject = World->SpawnActor<APoolingObjectBase>(
+		PoolingObjectClass, 
+		Owner->GetActorTransform(),		// 오너의 트랜스폼을 사용해서 배치
+		SpawnParams
+	);	// PoolingObjectClass를 생성
+
+	if (NewPoolObject)
+	{
+		NewPoolObject->OnObjectDeactivate.AddDynamic(this, 
+			&UObjectPoolActorComponent::SendToPool);	// 비활성화되면 SendToPool 함수 호출
+		NewPoolObject->Deactivate();					// 생성된 오브젝트를 비활성화
+	}
+
+	return NewPoolObject;
+}
+
+void UObjectPoolActorComponent::SendToPool(APoolingObjectBase* Object)
+{
+	AActor* Owner = GetOwner();	// 이 컴포넌트가 들어있는 액터를 가져오기
+	if (Owner && Object)
+	{
+		Object->AttachToActor(Owner, FAttachmentTransformRules::KeepWorldTransform);	// 오너에 붙이기
+		Object->SetActorLocation(Owner->GetActorLocation() + LocationOffset);			// 위치 설정(오너위치 + LocationOffset)
+	}
+	Pool.Add(Object);			// Pool에 추가
 }
 
--- ShootingCPP/Source/ShootingCPP/ObjectPoolActorComponent.h
+++ ShootingCPP/Source/ShootingCPP/ObjectPoolActorComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PoolingObjectBase.h"
 #include "ObjectPoolActorComponent.generated.h"
 
 
@@ -20,9 +21,36 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+	// 풀에서 오브젝트를 가져오는 함수(트랜스폼을 지정하여 오브젝트를 가져올 수 있다, 블루프린트에서 호출 가능)
+	UFUNCTION(BlueprintCallable, Category = "Object Pool")
+	APoolingObjectBase* GetObject(const FTransform& Transform);
 
-		
+	// 풀에서 오브젝트를 가져오는 함수(C++ 전용. 트랜스폼은 Identity를 사용.)
+	APoolingObjectBase* GetObject();
+
+protected:
+	// 풀에 오브젝트가 없을 때 새로 생성하는 함수
+	APoolingObjectBase* SpawnNewPooledObject();
+
+	// 오브젝트를 풀 액터에 어태치하고 풀로 되돌리는 함수. 델리게이트에 연결을 위해 UFUNCTION() 추가
+	UFUNCTION()
+	void SendToPool(APoolingObjectBase* Object);
+
+public:
+	// 풀링 오브젝트의 클래스를 지정(APoolingObjectBase의 파생 클래스를 저장한다.)
+	UPROPERTY(EditDefaultsOnly, Category = "Object Pool")
+	TSubclassOf<APoolingObjectBase> PoolingObjectClass = nullptr;
+
+	// 풀의 초기 크기
+	UPROPERTY(EditDefaultsOnly, Category = "Object Pool")
+	int32 InitialPoolSize = 5;
+
+protected:
+	// 풀링 되는 오브젝트들의 배열(전부 비활성화 된 오브젝트만 들어있다.)
+	TArray<APoolingObjectBase*> Pool;
+
+	// 풀에서 비활성화된 오브젝트의 위치 오프셋 
+	UPROPERTY(EditDefaultsOnly, Category = "Object Pool")
+	FVector LocationOffset = FVector(0.f, 0.f, 10000.f);
 };
