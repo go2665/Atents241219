522770ccffe79abe8fd17f30a40ee00aa9c3d7a6
go2665 <77224762+go2665@users.noreply.github.com>
2025-02-18 09:54:28 +0900

    ShootingCPP - PoolingEnemy_Bullet 완료

--- ShootingCPP/Source/ShootingCPP/PoolingEnemy_Bullet.cpp
+++ ShootingCPP/Source/ShootingCPP/PoolingEnemy_Bullet.cpp
@@ -2,4 +2,85 @@
 
 
 #include "PoolingEnemy_Bullet.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
+APoolingEnemy_Bullet::APoolingEnemy_Bullet()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BulletMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BulletMesh"));
+	BulletMesh->SetupAttachment(RootComponent);
+	BulletMesh->SetCollisionProfileName(TEXT("OverlapAllDynamic"));	// 오버랩만 사용
+
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow->SetupAttachment(RootComponent);
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(
+		TEXT("ProjectileMovement"));
+	ProjectileMovement->ProjectileGravityScale = 0.0f;	// 중력 적용 안함
+	ProjectileMovement->bConstrainToPlane = true;		// XY평면에 제한
+	ProjectileMovement->SetPlaneConstraintNormal(FVector(0.0f, 0.0f, 1.0f));	// Z축이 노멀벡터
+}
+
+void APoolingEnemy_Bullet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 오버랩 이벤트 때 실행될 함수 설정
+	OnActorBeginOverlap.AddDynamic(this, &APoolingEnemy_Bullet::OnBulletOverlapBegin);
+}
+
+void APoolingEnemy_Bullet::OnActivate()
+{
+	Super::OnActivate();
+
+	ProjectileMovement->Velocity = MoveSpeed * GetActorForwardVector();	// 앞으로 이동하게 설정
+	BulletMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);		// 충돌 활성화
+	BulletMesh->SetVisibility(true);									// 보이게 설정
+
+	FTimerManager& TimerManager = GetWorldTimerManager();
+	TimerManager.SetTimer(
+		LifeTimeTimer,
+		this, &APoolingEnemy_Bullet::Deactivate,
+		LifeTime, false);	// 수명이 다되면 Deactivate
+}
+
+void APoolingEnemy_Bullet::OnDeactivate()
+{
+	Super::OnDeactivate();
+
+	FTimerManager& TimerManager = GetWorldTimerManager();
+	TimerManager.ClearTimer(LifeTimeTimer);	// 타이머 정리
+	LifeTimeTimer.Invalidate();				// 타이머 무효화
+
+	ProjectileMovement->Velocity = FVector::ZeroVector;		// 이동 중지
+
+	BulletMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);	// 충돌 비활성화
+	BulletMesh->SetVisibility(false);									// 보이지 않게 설정
+
+	if (bIsInitialized)
+	{
+		// 첫번째 Deactivate는 제외하고 그 이후부터 폭발 이팩트 생성
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			GetWorld(),
+			SelfExplosionEffect,
+			BulletMesh->GetComponentLocation(), FRotator::ZeroRotator, FVector(1.0f),
+			true, true, ENCPoolMethod::AutoRelease, true);
+	}
+	else
+	{
+		// 첫번째는 Pool에 들어가는 거니까 폭발 이팩트는 생성하지 않음
+		bIsInitialized = true;
+	}
+}
+
+void APoolingEnemy_Bullet::OnBulletOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor->ActorHasTag("Player"))	// 플레이어와 겹친 경우
+	{
+		UGameplayStatics::ApplyDamage(
+			OtherActor, Damage, nullptr, nullptr, nullptr);	// 데미지 적용하고
+		Deactivate();						// 비활성화
+	}
+}
--- ShootingCPP/Source/ShootingCPP/PoolingEnemy_Bullet.h
+++ ShootingCPP/Source/ShootingCPP/PoolingEnemy_Bullet.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "PoolingObjectBase.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "NiagaraSystem.h"
 #include "PoolingEnemy_Bullet.generated.h"
 
 /**
@@ -14,4 +17,54 @@ class SHOOTINGCPP_API APoolingEnemy_Bullet : public APoolingObjectBase
 {
 	GENERATED_BODY()
 	
+public:
+	APoolingEnemy_Bullet();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnActivate() override;
+	virtual void OnDeactivate() override;
+
+private:
+	// 총알이 무언가와 겹쳤을 때 호출되는 함수
+	UFUNCTION()
+	void OnBulletOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+protected:
+	// 총알 메쉬
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	UStaticMeshComponent* BulletMesh = nullptr;
+
+	// 총알 포워드 표시용	화살표
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	UArrowComponent* Arrow = nullptr;
+
+	// 총알 이동 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
+
+protected:
+	// 총알 이동 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float MoveSpeed = 200.0f;
+
+	// 총알의 수명
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float LifeTime = 20.0f;
+
+	// 총알이 플레이어에게 주는 데미지
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float Damage = 3.0f;
+
+	// 총알이 터지는 이펙트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	UNiagaraSystem* SelfExplosionEffect = nullptr;
+
+private:
+	// 초기화 여부(첫번째 비활성화 때 터지는 이팩트 나오는 것 방지용)
+	bool bIsInitialized = false;
+
+	// 수명 체크용 타이머
+	UPROPERTY()
+	FTimerHandle LifeTimeTimer;
 };
