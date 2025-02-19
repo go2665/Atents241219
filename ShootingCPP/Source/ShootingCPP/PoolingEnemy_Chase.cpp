86d304d9a0d1eaf1145325494cb3b6ca82723028
go2665 <77224762+go2665@users.noreply.github.com>
2025-02-19 09:49:09 +0900

    ShootingCPP - PoolingEnemy_Chase 작성 완료

--- ShootingCPP/Source/ShootingCPP/PoolingEnemy_Chase.cpp
+++ ShootingCPP/Source/ShootingCPP/PoolingEnemy_Chase.cpp
@@ -6,7 +6,7 @@
 
 APoolingEnemy_Chase::APoolingEnemy_Chase()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;	
 }
 
 void APoolingEnemy_Chase::Tick(float DeltaTime)
@@ -15,11 +15,27 @@ void APoolingEnemy_Chase::Tick(float DeltaTime)
 
 	if (TargetPawn)
 	{
-		BehaviorPhase1(DeltaTime);
+		// 페이즈1: 플레이어가 살아있을 때, 페이즈1 시간이 남았을 때
+		if (!bIsPhase1)
+		{
+			bIsPhase1 = true;	// DoOnce용도
+			//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::Printf(TEXT("Phase1 Start : %.1f"), GetWorld()->GetTimeSeconds()));
+			ProjectileMovement->bForceSubStepping = false;	// 서브 스텝 강제 해제(천천히 움직일 때는 필요없음)
+			DamagePerSecond = Phase1Damage;	// 페이즈1 데미지 설정
+		}
+		BehaviorPhase1(DeltaTime);	// 페이즈1 행동처리
 	}
 	else
 	{
-		BehaviorPhase2(DeltaTime);
+		// 페이즈2: 플레이어가 죽었을 때, 페이즈1 시간이 다되었을 때 진입
+		if (!bIsPhase2)
+		{
+			bIsPhase2 = true;
+			//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green, FString::Printf(TEXT("Phase2 Start : %.1f"), GetWorld()->GetTimeSeconds()));
+			ProjectileMovement->bForceSubStepping = true;	// 서브 스텝 강제(더 상세한 물리 계산)
+			DamagePerSecond = Phase2Damage;	// 페이즈2 데미지 설정
+		}
+		BehaviorPhase2(DeltaTime);	// 페이즈2 행동처리
 	}
 }
 
@@ -29,47 +45,58 @@ void APoolingEnemy_Chase::BeginPlay()
 	UWorld* World = GetWorld();
 	APlayerController* PlayerController = World->GetFirstPlayerController();
 	APlayerWingUsePool* Wing = Cast<APlayerWingUsePool>(PlayerController->GetPawn());
-	Wing->OnDie.AddDynamic(this, &APoolingEnemy_Chase::TargetLost);
+	Wing->OnDie.AddDynamic(this, &APoolingEnemy_Chase::TargetLost);	// 플레이어 사망시 TargetPawn을 nullptr로 설정
+
+	ProjectileMovement->MaxSpeed = MaxSpeed;	// 최대 속도 설정
 }
 
 void APoolingEnemy_Chase::OnActivate()
 {
 	Super::OnActivate();
 
+	SetActorTickEnabled(true);					// 틱 활성화
+
 	UWorld* World = GetWorld();
 	APlayerController* PlayerController = World->GetFirstPlayerController();
-	TargetPawn = PlayerController->GetPawn();	
+	TargetPawn = PlayerController->GetPawn();	// TargetPawn 플레이어 폰으로 설정
 
 	FTimerManager& TimerManager = World->GetTimerManager();
 	FTimerHandle TempHandle;
 	TimerManager.SetTimer(
 		TempHandle,
 		[this]() { TargetPawn = nullptr; },
-		Phase1Duration, false);
+		Phase1Duration, false);			// 페이즈1 지속 시간이 끝나면 페이즈2로 진입(TargetPawn을 nullptr로 설정)
 }
 
 void APoolingEnemy_Chase::OnDeactivate()
 {
 	Super::OnDeactivate();
 
-	TargetPawn = nullptr;
+	SetActorTickEnabled(false);	// 틱 비활성화
+	TargetPawn = nullptr;		// TargetPawn을 nullptr로 설정
+
+	bIsPhase1 = false;			// 페이즈1 진입용 변수 초기화
+	bIsPhase2 = false;			// 페이즈2 진입용 변수 초기화
 }
 
 void APoolingEnemy_Chase::BehaviorPhase1(float DeltaTime)
 {
-	FRotator Current = GetActorRotation();
+	FRotator Current = GetActorRotation();	// 현재 회전값
 	FVector Direction = TargetPawn->GetActorLocation() - GetActorLocation();
 	Direction.Z = 0;
-	FRotator Target = FRotationMatrix::MakeFromX(Direction).Rotator();
+	FRotator Target = FRotationMatrix::MakeFromX(Direction).Rotator();	// 플레이어를 바라보는 회전값
 
-	FRotator NewRotator = FMath::RInterpConstantTo(Current, Target, DeltaTime, RotateSpeed);
-	SetActorRotation(NewRotator);
+	FRotator NewRotator = FMath::RInterpConstantTo(Current, Target, DeltaTime, RotateSpeed);	// 회전 보간
+	SetActorRotation(NewRotator);			// 보간 결과 적용
 
-	ProjectileMovement->Velocity = MoveSpeed * GetActorForwardVector();
+	ProjectileMovement->Velocity = MoveSpeed * GetActorForwardVector();	// 회전 된 앞쪽으로 이동
 }
 
 void APoolingEnemy_Chase::BehaviorPhase2(float DeltaTime)
 {
 	// 앞쪽으로 가속
-	// 최대 속도까지 가속
+	ProjectileMovement->Velocity += Acceleration * DeltaTime * GetActorForwardVector();
+
+	// 벨로시티의 크기 출력
+	//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, FString::Printf(TEXT("Velocity : %.1f"), ProjectileMovement->Velocity.Size()));
 }
--- ShootingCPP/Source/ShootingCPP/PoolingEnemy_Chase.h
+++ ShootingCPP/Source/ShootingCPP/PoolingEnemy_Chase.h
@@ -25,6 +25,8 @@ protected:
 	virtual void OnDeactivate() override;
 
 private:
+	// 플레이어 사망시 원할한 처리를 위한 함수
+	UFUNCTION()
 	inline void TargetLost() { TargetPawn = nullptr; };
 
 	// 플레이어를 계속 바라보는 함수
@@ -34,12 +36,38 @@ private:
 	void BehaviorPhase2(float DeltaTime);	
 
 protected:
+	// 추적할 플레이어
 	UPROPERTY()
 	APawn* TargetPawn = nullptr;
 
+	// 페이즈1에서의 회전 속도
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
 	float RotateSpeed = 180.0f;
 
+	// 페이즈1 지속 시간
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
 	float Phase1Duration = 5.0f;
+
+	// 최대 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float MaxSpeed = 1500.0f;
+
+	// 페이즈2에서의 가속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float Acceleration = 500.0f;
+
+	// 페이즈1에서의 데미지
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float Phase1Damage = 10.0f;
+
+	// 페이즈2에서의 데미지
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pooling Enemy")
+	float Phase2Damage = 500.0f;
+
+private:
+	// 페이즈1 진입했을 때 한번 실행하기 위한 불리언 변수	
+	bool bIsPhase1 = false;
+
+	// 페이즈2 진입했을 때 한번 실행하기 위한 불리언 변수
+	bool bIsPhase2 = false;
 };
