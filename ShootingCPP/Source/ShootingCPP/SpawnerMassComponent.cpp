9e326d0a2d786c5694d0168cbc8744cec8f6b109
go2665 <77224762+go2665@users.noreply.github.com>
2025-02-21 13:12:47 +0900

    ShootingCPP - Mass 스포너 작성(주석추가)

--- ShootingCPP/Source/ShootingCPP/SpawnerMassComponent.cpp
+++ ShootingCPP/Source/ShootingCPP/SpawnerMassComponent.cpp
@@ -7,27 +7,28 @@ void USpawnerMassComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// CurveInputGap 미리 계산하기
+	// CurveInputGap(커브 입력의 간격) 미리 계산하기
 	if (MassCount < 2)
 	{
 		CurveInputGap = 0.5f;	// 2개 이하일 경우 0.5로 설정
 	}
 	else
 	{
-		CurveInputGap = 1.0f / (float)(MassCount - 1);	
+		CurveInputGap = 1.0f / (float)(MassCount - 1);	// 2개 이상일 경우 1/(MassCount-1)로 설정 (0에서 시작 1에서 끝나기 위해)
 	}
 }
 
 void USpawnerMassComponent::Spawn()
 {
+	// MassCount만큼 적 생성
 	for (int32 i = 0; i < MassCount; i++)
 	{
 		FTransform SpawnTransform = GetSpawnTransform();	
-		SpawnIndex++;
+		SpawnIndex++;	// 트랜스폼을 가져올 때마다 인덱스 증가
 		GameMode->GetEnemy(SpawnEnemyType, SpawnTransform);
 	}
 
-	SpawnIndex = 0;
+	SpawnIndex = 0;		// 다시 초기화
 }
 
 FTransform USpawnerMassComponent::GetSpawnTransform() const
@@ -35,16 +36,19 @@ FTransform USpawnerMassComponent::GetSpawnTransform() const
 	FTransform Result = FTransform();
 	if (SpawnArea)
 	{
-		FVector SpawnLocation;
-		SpawnLocation.X = (FormationCurve->GetFloatValue(CurveInputGap * SpawnIndex) * Offset.X - Offset.X);
-		SpawnLocation.Y = (CurveInputGap * SpawnIndex * Offset.Y);
-		SpawnLocation.Z = 0.0f;
+		FVector LocaLocation;
+		// X 결과는 0 ~ -Offset.X
+		LocaLocation.X = (FormationCurve->GetFloatValue(CurveInputGap * SpawnIndex) * Offset.X - Offset.X);	
+		// Y 결과는 -Offset.Y * 0.5 ~ Offset.Y * 0.5
+		LocaLocation.Y = -Offset.Y * 0.5f + (CurveInputGap * SpawnIndex * Offset.Y);
+		LocaLocation.Z = 0.0f;
 
+		// SpawnArea의 Forward 방향을 기준으로 회전시키는 회전 만들기
 		FRotator ForwardRotation = SpawnArea->GetForwardVector().ToOrientationRotator();
-		SpawnLocation = ForwardRotation.RotateVector(SpawnLocation);
+		LocaLocation = ForwardRotation.RotateVector(LocaLocation);	// 구했던 로컬 위치를 회전
 
 		FVector BaseLocation = SpawnArea->GetComponentLocation();
-		Result.SetLocation(BaseLocation + SpawnLocation);
+		Result.SetLocation(BaseLocation + LocaLocation);	// SpawnArea 위치에 계산이 끝난 로컬 위치 더하기
 		Result.SetRotation(SpawnArea->GetComponentQuat());
 		Result.SetScale3D(SpawnArea->GetComponentScale());
 	}
