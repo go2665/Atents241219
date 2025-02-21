ef21d03ffaefcc8030bd4963b5e554157eaee2ed
go2665 <77224762+go2665@users.noreply.github.com>
2025-02-21 14:31:50 +0900

    ShootingCPP - 빈 사용자 위젯 추가

---
+++ ShootingCPP/Source/ShootingCPP/CppShooterUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CppShooterUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGCPP_API UCppShooterUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
