35d59aa6b8eea761f23091b086d8892bd406fd5b
go2665 <77224762+go2665@users.noreply.github.com>
2025-03-24 10:20:32 +0900

    Action - 적 공격용 몽타주 재생, 비헤이비어 트리 수정

--- Action/Source/Action/Enemy/NormalEnemy.h
+++ Action/Source/Action/Enemy/NormalEnemy.h
@@ -23,4 +23,11 @@ public:
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
 	float MaxWalkSpeed = 400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack")
+	class UAnimMontage* AttackMontage = nullptr;
 };
+
+// 적의 공격이 플레이어에게 데미지를 입히도록 구현
+//	1. 데미지량은 적의 무기에 설정
+//  2. AnimNotifyState를 사용하여 무기의 충돌을 활성화/비활성화
