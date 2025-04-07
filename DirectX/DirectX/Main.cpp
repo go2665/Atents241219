b18a0c5f63735628e6cf716e3d4c17052caaf66e
go2665 <77224762+go2665@users.noreply.github.com>
2025-04-07 11:34:25 +0900

    DX - Main 수정

--- DirectX/DirectX/Main.cpp
+++ DirectX/DirectX/Main.cpp
@@ -29,10 +29,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		//Chap6App theApp(hInstance);
 		//Chap7_1App theApp(hInstance);
 		//Chap7_2App theApp(hInstance);
-		Chap8_1App theApp(hInstance);
+		//Chap8_1App theApp(hInstance);
 		//Chap8_2App theApp(hInstance);
 		//Chap9_1App theApp(hInstance);
-		//Chap9_2App theApp(hInstance);
+		Chap9_2App theApp(hInstance);
 
 		if (!theApp.Initialize())	// 초기화(윈도우 생성, DirectX 초기화)
 			return 0;
