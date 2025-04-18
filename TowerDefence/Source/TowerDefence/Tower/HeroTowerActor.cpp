44f304c2700bea83b2cccf0cf9dac3abe36d83de
go2665 <77224762+go2665@users.noreply.github.com>
2025-04-18 10:29:31 +0900

    TowerDefence - AreaIndicator 작성중

--- TowerDefence/Source/TowerDefence/Tower/HeroTowerActor.cpp
+++ TowerDefence/Source/TowerDefence/Tower/HeroTowerActor.cpp
@@ -1,74 +1,135 @@
 #include "HeroTowerActor.h"  
-#include "Engine/OverlapResult.h" 
 #include "Kismet/GameplayStatics.h"
 #include "TowerDefence/Enemy/EnemyBase.h"  
+#include "TowerDefence/Shot/Debuff/DebuffComponent.h"
 
 void AHeroTowerActor::UseSkill(FVector InLocation)  
 {  
    // 스킬 사용  
    USkillDataAsset* SkillDataAsset = SkillDataAssets[GunLevel - 1]; // 스킬 데이터 에셋을 가져온다.  
-   TArray<AEnemyBase*> Enemies;  
-   FindActorsInRadius(InLocation, SkillDataAsset->Radius, Enemies); // 반지름 안에 있는 적을 찾는다.  
-   UE_LOG(LogTemp, Warning, TEXT("[%s] : Skill Radius %.1f"), *GetActorNameOrLabel(), SkillDataAsset->Radius);
+     
+   //UE_LOG(LogTemp, Warning, TEXT("[%s] : Skill Radius %.1f"), *GetActorNameOrLabel(), SkillDataAsset->Radius);
 
    if (SkillDataAsset)  
    {  
-       if (SkillDataAsset->Damage > 0)  
+       if (SkillDataAsset->TowerBuffDataAssets.Num() > 0)  
        {  
-           // 데미지 스킬(InLocation에서 반지름 Radius안에 있는 AEnemyBase*에 데미지 주기)  
-		   for (AEnemyBase* Enemy : Enemies)
+           // 타워를 찾아야 하는 경우
+		   TArray<ATowerBaseActor*> Towers;
+           FindActorsInRadius(InLocation, SkillDataAsset->Radius, Towers);
+
+           // 버프 스킬  
+		   for (ATowerBaseActor* Tower : Towers)
 		   {
-			   if (Enemy)
+			   if (Tower)
 			   {
-				   UGameplayStatics::ApplyDamage(
-                       Enemy, SkillDataAsset->Damage, 
-                       nullptr, this, SkillDataAsset->DamageType);
+				   // 버프 적용
+				   for (UTowerBuffDataAsset* BuffDataAsset : SkillDataAsset->TowerBuffDataAssets)
+				   {
+					   Tower->AddBuff(BuffDataAsset);
+				   }
 			   }
 		   }
        }  
+       else
+       {
+		   // 적을 찾아야 하는 경우
+           TArray<AEnemyBase*> Enemies;
+           FindActorsInRadius(InLocation, SkillDataAsset->Radius, Enemies); // 반지름 안에 있는 적을 찾는다.
 
-       if (SkillDataAsset->TowerBuffDataAssets.Num() > 0)  
-       {  
-           // 버프 스킬  
-       }  
+           if (SkillDataAsset->Damage > 0)
+           {
+               // 데미지 스킬(InLocation에서 반지름 Radius안에 있는 AEnemyBase*에 데미지 주기)  
+               for (AEnemyBase* Enemy : Enemies)
+               {
+                   if (Enemy)
+                   {
+                       UGameplayStatics::ApplyDamage(
+                           Enemy, SkillDataAsset->Damage,
+                           nullptr, this, SkillDataAsset->DamageType);
+                   }
+               }
+           }
 
-       if (SkillDataAsset->DebuffTypes.Num() > 0)  
-       {  
-           // 디버프 스킬  
-       }  
+           if (SkillDataAsset->DebuffTypes.Num() > 0)
+           {
+               // 디버프 스킬  
+               for (AEnemyBase* Enemy : Enemies)
+               {
+                   for (EDebuffType DebuffType : SkillDataAsset->DebuffTypes)
+                   {
+                       Enemy->GetDebuffComponent()->AddDebuff(DebuffType, SkillDataAsset->DebuffModifier);
+                   }
+               }
+           }
+       }       
    }  
 }  
 
-void AHeroTowerActor::FindActorsInRadius(FVector Center, float Radius, TArray<AEnemyBase*>& OutActors)  
-{  
-    // 출력 파라메터 초기화
-    OutActors.Empty();
-
-    // 결과를 저장할 배열  
-    TArray<FOverlapResult> OverlapResults;  
-
-    // 월드에서 Overlap 검사  
-    bool bHasOverlaps = GetWorld()->OverlapMultiByObjectType(  
-        OverlapResults,  
-        Center,  
-        FQuat::Identity,  
-        FCollisionObjectQueryParams(ECollisionChannel::ECC_GameTraceChannel1),   
-        FCollisionShape::MakeSphere(Radius)  
-    );  
-
-    // 결과 처리  
-    if (bHasOverlaps)  
-    {  
-        for (const FOverlapResult& Result : OverlapResults)  
-        {  
-            AEnemyBase* Enemy = Cast<AEnemyBase>(Result.GetActor());  
-            if (Enemy)  
-            {  
-                OutActors.Add(Enemy);  
-            }  
-        }  
-    }  
-
-    // 디버그용으로 구체를 그려줌 (선택 사항)  
-    DrawDebugSphere(GetWorld(), Center, Radius, 12, FColor::Green, false, 2.0f);  
-}
+//void AHeroTowerActor::FindEnemiesInRadius(FVector Center, float Radius, TArray<AEnemyBase*>& OutActors)  
+//{  
+//    // 출력 파라메터 초기화
+//    OutActors.Empty();
+//
+//    // 결과를 저장할 배열  
+//    TArray<FOverlapResult> OverlapResults;  
+//
+//    // 월드에서 Overlap 검사  
+//    bool bHasOverlaps = GetWorld()->OverlapMultiByObjectType(  
+//        OverlapResults,  
+//        Center,  
+//        FQuat::Identity,  
+//        FCollisionObjectQueryParams(ECollisionChannel::ECC_GameTraceChannel1),   
+//        FCollisionShape::MakeSphere(Radius)  
+//    );  
+//
+//    // 결과 처리  
+//    if (bHasOverlaps)  
+//    {  
+//        for (const FOverlapResult& Result : OverlapResults)  
+//        {  
+//            AEnemyBase* Enemy = Cast<AEnemyBase>(Result.GetActor());  
+//            if (Enemy)  
+//            {  
+//                OutActors.Add(Enemy);  
+//            }  
+//        }  
+//    }  
+//
+//    // 디버그용으로 구체를 그려줌 (선택 사항)  
+//    DrawDebugSphere(GetWorld(), Center, Radius, 12, FColor::Green, false, 2.0f);  
+//}
+//
+//void AHeroTowerActor::FindTowersInRadius(FVector Center, float Radius, TArray<ATowerBaseActor*>& OutActors)
+//{
+//    // 출력 파라메터 초기화
+//    OutActors.Empty();
+//
+//    // 결과를 저장할 배열  
+//    TArray<FOverlapResult> OverlapResults;
+//
+//    // 월드에서 Overlap 검사  
+//    bool bHasOverlaps = GetWorld()->OverlapMultiByObjectType(
+//        OverlapResults,
+//        Center,
+//        FQuat::Identity,
+//        FCollisionObjectQueryParams(ECollisionChannel::ECC_GameTraceChannel2),
+//        FCollisionShape::MakeSphere(Radius)
+//    );
+//
+//    // 결과 처리  
+//    if (bHasOverlaps)
+//    {
+//        for (const FOverlapResult& Result : OverlapResults)
+//        {
+//            ATowerBaseActor* Enemy = Cast<ATowerBaseActor>(Result.GetActor());
+//            if (Enemy)
+//            {
+//                OutActors.Add(Enemy);
+//            }
+//        }
+//    }
+//
+//    // 디버그용으로 구체를 그려줌 (선택 사항)  
+//    DrawDebugSphere(GetWorld(), Center, Radius, 12, FColor::Green, false, 2.0f);
+//}
--- TowerDefence/Source/TowerDefence/Tower/HeroTowerActor.h
+++ TowerDefence/Source/TowerDefence/Tower/HeroTowerActor.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/OverlapResult.h" 
 #include "TowerBaseActor.h"
 #include "TowerDefence/Skill/SkillDataAsset.h"
 #include "HeroTowerActor.generated.h"
@@ -17,15 +18,67 @@ class TOWERDEFENCE_API AHeroTowerActor : public ATowerBaseActor
 	GENERATED_BODY()
 	
 public:
-	UFUNCTION(BlueprintCallable, Category = "Skill")
+	UFUNCTION(BlueprintCallable, Category = "Tower|Hero")
 	void UseSkill(FVector InLocation);
 
 private:
-	void FindActorsInRadius(FVector Center, float Radius, TArray<AEnemyBase*>& OutActors);
+	//void FindEnemiesInRadius(FVector Center, float Radius, TArray<AEnemyBase*>& OutActors);
+	//void FindTowersInRadius(FVector Center, float Radius, TArray<ATowerBaseActor*>& OutActors);
+
+	// T 타입에 해당하는 적이나 타워를 찾는 함수
+	template<typename T>
+	void FindActorsInRadius(FVector Center, float Radius, TArray<T*>& OutActors)
+	{
+		// 출력 파라메터 초기화
+		OutActors.Empty();
+		// 결과를 저장할 배열  
+		TArray<FOverlapResult> OverlapResults;
+		
+		// T 타입에 따라 적이나 타워의 충돌 채널을 설정
+		ECollisionChannel CollisionChannel;
+		if (std::is_same<T, AEnemyBase>::value)
+		{
+			CollisionChannel = ECollisionChannel::ECC_GameTraceChannel1; // 적
+		}
+		else if (std::is_same<T, ATowerBaseActor>::value)
+		{
+			CollisionChannel = ECollisionChannel::ECC_GameTraceChannel2; // 타워
+		}
+		else
+		{
+			return;
+		}
+		
+		// 월드에서 Overlap 검사  
+		bool bHasOverlaps = GetWorld()->OverlapMultiByObjectType(
+			OverlapResults,
+			Center,
+			FQuat::Identity,
+			FCollisionObjectQueryParams(CollisionChannel),
+			FCollisionShape::MakeSphere(Radius)
+		);
+		if (bHasOverlaps)
+		{
+			for (const FOverlapResult& Result : OverlapResults)
+			{
+				if (AActor* Actor = Result.GetActor())
+				{
+					if (T* CastedActor = Cast<T>(Actor))
+					{
+						OutActors.Add(CastedActor);
+					}
+				}
+			}
+		}
+
+		// 디버그용으로 구체를 그려줌 (선택 사항)  
+	    DrawDebugSphere(GetWorld(), Center, Radius, 12, FColor::Green, false, 2.0f);
+	}
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower|Hero")
 	TArray<USkillDataAsset*> SkillDataAssets;
 
 	
 };
+
